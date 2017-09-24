4e74062505515d2baf038a307cf890d460b556df
Valter Andrei <valterandrey@gmail.com>
2017-09-24 18:16:26 -0300

    check valid masks

--- masks.js
+++ masks.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
   var valid_cnpj =  {
     onComplete: function(cnpj){
-      console.log('CNPJ Válido! ', cnpj);
+      if (isCNPJ(cnpj)){
+        console.log('CNPJ válido.');
+      }else {
+        console.log('CNPJ inválido.');
+      }
     }
   };
 
   var valid_cpf =  {
     onComplete: function(cpf){
-      isCPF(cpf)
+      if (isCPF(cpf)){
+        console.log('CPF válido.');
+      }else {
+        console.log('CPF inválido.');
+      }
     },
     reverse: true
   };
@@ -24,36 +32,87 @@ $(document).ready(function(){
     }
   });
 
-  function isCPF(value){
-    var cpf = value.replace(/[^0-9]/g, '').toString();
+  /*****************************
+  VALIDA O CNPJ
+  *****************************/
+  function isCNPJ(cnpj) {
+      cnpj = cnpj.replace(/[^\d]+/g, '');
 
-    if( cpf.length == 11 )
-    {
-        var v = [];
+      if (cnpj == '00000000000000' ||
+          cnpj == '11111111111111' ||
+          cnpj == '22222222222222' ||
+          cnpj == '33333333333333' ||
+          cnpj == '44444444444444' ||
+          cnpj == '55555555555555' ||
+          cnpj == '66666666666666' ||
+          cnpj == '77777777777777' ||
+          cnpj == '88888888888888' ||
+          cnpj == '99999999999999')
+          return false;
 
-        //Calcula o primeiro dígito de verificação.
-        v[0] = 1 * cpf[0] + 2 * cpf[1] + 3 * cpf[2];
-        v[0] += 4 * cpf[3] + 5 * cpf[4] + 6 * cpf[5];
-        v[0] += 7 * cpf[6] + 8 * cpf[7] + 9 * cpf[8];
-        v[0] = v[0] % 11;
-        v[0] = v[0] % 10;
+      tamanho = cnpj.length - 2
+      numeros = cnpj.substring(0, tamanho);
+      digitos = cnpj.substring(tamanho);
+      soma = 0;
+      pos = tamanho - 7;
+      for (i = tamanho; i >= 1; i--) {
+          soma += numeros.charAt(tamanho - i) * pos--;
+          if (pos < 2)
+              pos = 9;
+      }
+      resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+      if (resultado != digitos.charAt(0))
+          return false;
 
-        //Calcula o segundo dígito de verificação.
-        v[1] = 1 * cpf[1] + 2 * cpf[2] + 3 * cpf[3];
-        v[1] += 4 * cpf[4] + 5 * cpf[5] + 6 * cpf[6];
-        v[1] += 7 * cpf[7] + 8 * cpf[8] + 9 * v[0];
-        v[1] = v[1] % 11;
-        v[1] = v[1] % 10;
+      tamanho = tamanho + 1;
+      numeros = cnpj.substring(0, tamanho);
+      soma = 0;
+      pos = tamanho - 7;
+      for (i = tamanho; i >= 1; i--) {
+          soma += numeros.charAt(tamanho - i) * pos--;
+          if (pos < 2)
+              pos = 9;
+      }
+      resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+      if (resultado != digitos.charAt(1))
+          return false;
 
-        //Retorna Verdadeiro se os dígitos de verificação são os esperados.
-        if ( (v[0] != cpf[9]) || (v[1] != cpf[10]) )
-        {
-          console.log('CPF inválido: ' + cpf);
-        }
-    }
-    else
-    {
-      console.log('CPF inválido:' + cpf);
-    }
+      return true;
+  }
+
+  /*****************************
+  VALIDA O CPF
+  *****************************/
+  function isCPF(strCPF) {
+      var Soma;
+      var Resto;
+      var cpf = strCPF.replace(/[^0-9]/g, '').toString();
+
+      Soma = 0;
+  	if (cpf == '00000000000' ||
+        cpf == '11111111111' ||
+        cpf == '22222222222' ||
+        cpf == '33333333333' ||
+        cpf == '44444444444' ||
+        cpf == '55555555555' ||
+        cpf == '66666666666' ||
+        cpf == '77777777777' ||
+        cpf == '88888888888' ||
+        cpf == '99999999999'
+        ) return false;
+
+  	for (i=1; i<=9; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+  	Resto = (Soma * 10) % 11;
+
+      if ((Resto == 10) || (Resto == 11))  Resto = 0;
+      if (Resto != parseInt(cpf.substring(9, 10)) ) return false;
+
+  	Soma = 0;
+      for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+      Resto = (Soma * 10) % 11;
+
+      if ((Resto == 10) || (Resto == 11))  Resto = 0;
+      if (Resto != parseInt(cpf.substring(10, 11) ) ) return false;
+      return true;
   }
 });
