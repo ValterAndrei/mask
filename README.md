9e5c032e22399af711e7c0b96869b00827e3f86d
Valter Andrei <valterandrey@gmail.com>
2017-10-18 22:53:26 -0200

    add new masks

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# Máscaras para validar CPF e CNPJ - jQuery
+# Máscaras - jQuery
 
 [Documentação](https://igorescobar.github.io/jQuery-Mask-Plugin/docs.html)
 
+[Documentação - maskMoney](http://plentz.github.io/jquery-maskmoney/)
+
 [Demo](https://htmlpreview.github.io/?https://github.com/ValterAndrei/mask/blob/master/index.html)
+
+
+Recebendo parâmetros da maskMoney em Ruby on Rails:
+
+```
+params_result[:value] = params_result[:value].gsub(/\./, '').gsub(/,/, '.').to_f
+```
--- masks.js
+++ masks.js
@@ -37,10 +37,20 @@ $(document).ready(function(){
     }
   };
 
+  var SPMaskBehavior = function (val) {
+    return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+  },
+  spOptions = {
+    onKeyPress: function(val, e, field, options) {
+        field.mask(SPMaskBehavior.apply({}, arguments), options);
+      }
+  };
 
   $('.cnpj').mask('00.000.000/0000-00', valid_cnpj);
   $('.cpf').mask('000.000.000-00', valid_cpf);
   $('.date').mask('00/00/0000', valid_date);
+  $('.phone').mask(SPMaskBehavior, spOptions);
+  $('.money').maskMoney({allowNegative: true, thousands:'.', decimal:',', affixesStay: false});
 
 
   /*****************************
